000ce95685efde6802b65f01cae94550bc4e90b7
pedro01p <tsukki237@gmail.com>
2024-09-05 10:15:03 -0300

    nome completo

---
+++ atividade006.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+n=(input("insira seu primeiro nome"))
+s=(input("insira seu sobrenome"))
+process=(n+" "+s)
+print(process)
